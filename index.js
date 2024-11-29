d48eac47036ff21455fee23fe91075f73cfe3ba5
DanaRiabokon <riabokon.dana@gmail.com>
2024-11-29 14:18:18 +0100

    add index2.js, upd index.js

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ console.log(chalk.green.bold ('I like coding in JavaScript'));
 console.log(chalk.redBright.bold ('I like coding in JavaScript'));
 console.log(chalk.bgYellow.green.bold ('I like coding in JavaScript'));
 console.log(chalk.bgGreen.red.italic ('I like coding in JavaScript'));
+console.log(chalk.bgGreen.yellow.italic ('I like coding in JavaScript'));
 
 
---
+++ index2.js
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('I like coding in JavaScript'));
+console.log(chalk.green.bold ('I like coding in JavaScript'));
