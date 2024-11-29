f77088e2fbb125d7bea5eb8090c3fe19314c0a2f
DanaRiabokon <riabokon.dana@gmail.com>
2024-11-29 01:53:53 +0100

    commit homework3

---
+++ index.js
@@ -0,0 +1,9 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('I like coding in JavaScript'));
+console.log(chalk.green.bold ('I like coding in JavaScript'));
+console.log(chalk.redBright.bold ('I like coding in JavaScript'));
+console.log(chalk.bgYellow.green.bold ('I like coding in JavaScript'));
+console.log(chalk.bgGreen.red.italic ('I like coding in JavaScript'));
+
+
